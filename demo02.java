011561f28e02f11327c68f82d2ab80a4b6a76246
snehayadav-23 <snehayadav011200523@gmail.com>
2025-01-27 11:12:24 +0530

    Greatest among two numbers

---
+++ demo02.java
@@ -0,0 +1,26 @@
+public class demo02 {
+    public static void main(String[] args) {
+        // Define the range
+        int n = 100;
+
+        // Loop through numbers from 1 to n
+        for (int i = 1; i <= n; i++) {
+            // Check for FizzBuzz (divisible by both 3 and 5)
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            // Check for Fizz (divisible by 3)
+            else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            // Check for Buzz (divisible by 5)
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            // Otherwise, print the number
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+}
