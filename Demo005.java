9bb6ee3b66c8aac4a73ab035bdf61dcd50ee8cf6
snehayadav-23 <snehayadav011200523@gmail.com>
2025-02-07 12:27:33 +0530

    UserCredentials using while loop

---
+++ Demo005.java
@@ -0,0 +1,11 @@
+public class Demo005 {
+     public static void main(String[] args) {
+            String Username = "user001";
+            String pass ="user_001";
+            while((Username == "user001") && (pass == "user_001" )) {
+                System.out.println("Hello user Happy new year");
+                break;
+            }
+        
+        }
+    }
