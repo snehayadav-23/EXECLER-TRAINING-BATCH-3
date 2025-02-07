2fb850e4a01d332415773e114d44f659dad9bfed
snehayadav-23 <snehayadav011200523@gmail.com>
2025-02-07 12:36:39 +0530

    user credentials2 using while loop

---
+++ Demo006.java
@@ -0,0 +1,14 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class Demo006 {
+    public static void main(String[] args) {
+          String Username = "Snehayadav-011";
+          String Pass = "snehayadav";
+          while((Username == "Snehayadav-011") && (Pass == "snehayadav")) {
+              System.out.println("Welcome to github");
+              break;
+    }
+    }
+    }
+    
+              
+         
